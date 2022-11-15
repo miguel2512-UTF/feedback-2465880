aa5411c8ddd104b3a87a05c0b881416d65cb7de1
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2022-11-15 09:00:03 -0500

    Introducción React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const bootcamps = [
+        {
+            id: 1,
+            name: "PHP Bootcamp",
+            average_rating: 8
+        },
+        {
+            id: 1,
+            name: "Javascript Bootcamp",
+            average_rating: 10
+        }
+    ]
+
+    return(
+        <div>
+            <h1> Listado de Bootcamps </h1>
+            <ul>
+                { bootcamps.map((bootcamp, index)=>(
+                    <li key={index}> { bootcamp.name } </li>
+                 )) 
+                }
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
